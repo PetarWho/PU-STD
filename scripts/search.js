c39ace648ea5fa5e7be0dd6d044cd083ca018e82
Roy <92612927+PetarWho@users.noreply.github.com>
2023-01-09 23:15:55 +0200

    ST1 Exam Year 2

---
+++ scripts/search.js
@@ -0,0 +1,15 @@
+document.getElementById("form2").onkeypress = function (e) {
+    var key = e.charCode || e.keyCode || 0;
+    if (key == 13) {
+        e.preventDefault();
+        var query = document.getElementById('search').value;
+        window.find(query);
+        return true;
+    }
+}
+
+document.getElementById("form2").onclick = function (e) {
+    var query = document.getElementById('search').value;
+    window.find(query);
+    return true;
+}
